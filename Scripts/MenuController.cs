d405d1bde283a7d2b1eb497d8884a5a5702a7dc4
PyrrhicShadow <jrk135711@gmail.com>
2022-11-04 22:10:05 -0700

    Started on ending animations

---
+++ Scripts/MenuController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+    
+    [SerializeField]
+    private Animator startAnim;
+    [SerializeField]
+    private Animator endAnim;  
+
+    public void buttonOK() {
+        if (startAnim != null) {
+            startAnim.SetBool("show", true); 
+        }
+    }
+
+    public void newGame() {
+        if (endAnim != null) {
+            endAnim.SetBool("end", false); 
+            startAnim.SetBool("show", false); 
+        }
+    }
+
+    public void endGame() {
+        if (endAnim != null) {
+            endAnim.SetBool("end", true); 
+        }
+    }
+}
