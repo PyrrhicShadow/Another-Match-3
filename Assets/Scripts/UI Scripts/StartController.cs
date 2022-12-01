a5aab5b7ff021cd6cb4990af258096e9de0e4846
PyrrhicShadow <jrk135711@gmail.com>
2022-11-30 17:31:23 -0800

    Missing EventSystem on start page

--- Assets/Scripts/UI Scripts/StartController.cs
+++ Assets/Scripts/UI Scripts/StartController.cs
@@ -30,7 +30,7 @@ public class StartController : MonoBehaviour {
     }
 
     public void buttonStart() {
-        SceneManager.LoadScene(sceneToLoad); 
         Debug.Log("Next Scene"); 
+        SceneManager.LoadScene(sceneToLoad); 
     }
 }
